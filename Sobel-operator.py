d648365f60a83d06b5306652edb7d138e1f486c3
Wuelle <58120269+Wuelle@users.noreply.github.com>
2020-01-02 20:06:02 +0100

    Cleaned up the Code and removed some stuff
    Got rid of the entire "GetPixelValue" function and made the code look a lot cleaner by using the dot product instead of calculating every pixel value individually. The "SetPixel" function was also removed.

--- Sobel-operator.py
+++ Sobel-operator.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 import cv2
 import colorsys
+import sys
 """
 The Algorithm:
 	-https://www.youtube.com/watch?v=uihBwtPIBxM
@@ -15,52 +16,44 @@ Horizontal Kernel:
 -1 -2 -1
 """
 
-img = cv2.imread("TestImage.jpg")
-result = np.zeros([img.shape[0], img.shape[1], 3])
+img_raw = cv2.imread(sys.argv[1])
 
-def SetPixel(coords, color):
-	global img
-	result[coords[0], coords[1], 0] = color[2]
-	result[coords[0], coords[1], 1] = color[1]
-	result[coords[0], coords[1], 2] = color[0]
+#Add padding to the image
+img = np.zeros([img_raw.shape[0]+2, img_raw.shape[1]+2, img_raw.shape[2]])
+img[1:img_raw.shape[0]+1, 1:img_raw.shape[1]+1,] = img_raw
 
-def GetPixelValue(x, y):
-	#Pixels outside the image are returned as black
-	try: 
-		return np.average(img[x, y])
-	except:
-		return 0
+
+result = np.zeros([img_raw.shape[0], img_raw.shape[1], 3])
 
 def ApplyFilter(x, y):
-	Y_Kernel_Value = 0
-	Y_Kernel_Value +=  1 * GetPixelValue(x-1, y-1)
-	Y_Kernel_Value +=  2 * GetPixelValue(x-1, y)
-	Y_Kernel_Value +=  1 * GetPixelValue(x-1, y+1)
-	Y_Kernel_Value += -1 * GetPixelValue(x+1, y-1)
-	Y_Kernel_Value += -2 * GetPixelValue(x+1, y)
-	Y_Kernel_Value += -1 * GetPixelValue(x+1, y+1)
+	img_part = np.average(img[x-1:x+2, y-1:y+2], axis = 2)
+	y_kernel = np.array([
+		[1, 0, -1],
+		[2, 0, -2],
+		[1, 0, -1]])
 
-	X_Kernel_Value = 0
-	X_Kernel_Value +=  1 * GetPixelValue(x-1, y-1)
-	X_Kernel_Value +=  2 * GetPixelValue(x, y-1)
-	X_Kernel_Value +=  1 * GetPixelValue(x+1, y-1)
-	X_Kernel_Value += -1 * GetPixelValue(x-1, y+1)
-	X_Kernel_Value += -2 * GetPixelValue(x, y+1)
-	X_Kernel_Value += -1 * GetPixelValue(x+1, y+1)
+	x_kernel = np.array([
+		[ 1,  2,  1],
+		[ 0,  0,  0],
+		[-1, -2, -1]])
+
+	Y_Kernel_Value = np.dot(y_kernel.flatten(), img_part.flatten())
+	X_Kernel_Value = np.dot(x_kernel.flatten(), img_part.flatten())
 
 	return math.atan2(Y_Kernel_Value, X_Kernel_Value),math.sqrt(X_Kernel_Value**2+Y_Kernel_Value**2)
 
 #Calculate all the pixel values for the 'result' image
-for x in range(img.shape[0]):
-	for y in range(img.shape[1]):
+for x in range(1, img.shape[0]-1):
+	for y in range(1, img.shape[1]-1):
 		angle, value = ApplyFilter(x, y)
 		rgb_color = colorsys.hsv_to_rgb(angle, 1, value/(math.sqrt(2)*255))
-		SetPixel([x, y], rgb_color)
+		result[x-1, y-1] = rgb_color
+
 
 if img.shape[0] > img.shape[1]:
-	result_conc = np.concatenate((img/255, result), axis=1)
+	result_conc = np.concatenate((img_raw/255, result), axis=1)
 else: 
-	result_conc = np.concatenate((img/255, result), axis=0)
+	result_conc = np.concatenate((img_raw/255, result), axis=0)
 
 cv2.imwrite("Result.jpg", result*255)
 cv2.imshow("Result", result_conc)
